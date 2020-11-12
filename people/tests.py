12ef791825c17d8f2a918c2c81c11b73324a3ad8
Adit Negi <aditnegi1@gmail.com>
2020-11-12 23:26:40 +0530

    Update tests.py

--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from people.models import Person
+
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Person.objects.create(title="lion", email="roar@gmail.co",
+                              problem_descrition="dsfads", location="Pitampura")
+        Person.objects.create(title="cat", email="meow@adit.co",
+                              problem_descrition="dsfa", location="Pitampura")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Person.objects.get(title="lion")
+        cat = Person.objects.get(title="cat")
+        self.assertEqual(lion.title, 'lion')
+        self.assertEqual(cat.email, "meow@adit.co")
